968f3786b11c9444c19fdb4d7784b3a5ac71c196
José Daniel Sosa Lobo <jdsl.dev@proton.me>
2025-08-14 02:30:34 +0200

    Fix project routing to angular 20

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+];
